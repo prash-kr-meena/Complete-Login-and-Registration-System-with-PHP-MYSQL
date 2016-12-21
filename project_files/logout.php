77bcc1563a7121f4ea44aef74a49d70e80f55b8c
Prashant Kumar <prashantkr314@gmail.com>
2016-12-21 13:48:36 +0530

    added the sweet alert to the logout process too

--- project_files/logout.php
+++ project_files/logout.php
@@ -2,7 +2,12 @@
 include_once "resource/session.php";
 include_once 'resource/utilities.php';
 
-session_destroy();
-redirectTO('index'); 
+include_once 'partials/headers.php';# these are only added because of the echo the sctipt, (which needs header file as the linkage to the sweet alert files, are in the headers only)
 
+//redirectTO('index'); 
+# popupMessage($title, $text, $type, $page)
+echo popupMessage("Logout Successfull",'Hope you enjoyed it :)','success','index.php');	
+session_destroy();
+
+include_once "partials/footers.php";
 ?>
